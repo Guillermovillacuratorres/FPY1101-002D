b7c8f1699f358a4219cb8628a04fe55d30747916
Guillermovillacuratorres <127245210+Guillermovillacuratorres@users.noreply.github.com>
2025-04-25 11:22:17 -0400

    Clase 25-04-2025

---
+++ Clase 25-04-2025/if.py
@@ -0,0 +1,6 @@
+edad = 18
+
+if edad > 18 and edad < 10 or edad == 18:
+    print("Verdadero")
+else:
+    print("Falso")
---
+++ Clase 25-04-2025/if_anidados.py
@@ -0,0 +1,11 @@
+edad = 20
+nombre = "Juan"
+
+if edad == 18:
+    print("Tiene  18")
+elif edad <=10:
+    print("Es menor o igual a 10")
+elif (nombre == 'Juan'):
+    print(f"Su nombre es {nombre}")
+else:
+    print("no es nada")
